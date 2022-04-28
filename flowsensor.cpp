052de477107e65a8a6f6dbc06c8f43498cfe06f1
underratedprogrammer <103491599+underratedprogrammer@users.noreply.github.com>
2022-04-28 14:52:15 +0530

    Updated flowsensor code

--- flowsensor.cpp
+++ flowsensor.cpp
@@ -1,58 +1,73 @@
-byte sensorpin = 0;
+yte sensorpinSt = 0; //digitalpin 2
+byte sensorpinEn = 1; //digitalpin 3
 float calibrationfactor = 4.5;  //4.5 pluses / sec -- >> 1 l /min
 
 volatile byte pulsecount;
-float flowrate;
-unsigned int flowMilliliter;
-unsigned int totalmilliliters;
-
-unsigned int flowliter;
+float flowrateSt;
+unsigned int flowMilliliterSt;
+unsigned int totalmillilitersSt;
+unsigned int flowliterSt;
 unsigned long oldtime = 0;
+
+float flowrateEn;
+unsigned int flowMilliliterEn;
+unsigned int totalmillilitersEn;
+unsigned int flowliterEn;
 
 void setup() {
 Serial.begin(9600);
 pulsecount = 0;
-flowrate = 0;
-flowMilliliter = 0;
-
-flowliter = 0;
+flowrateSt = 0;
+flowMilliliterSt = 0;
+flowliterSt = 0;
 oldtime =0;
 
+flowrateEn = 0;
+flowMilliliterEn = 0;
+flowliterEn = 0;
+
 /// Interupt
-attachInterrupt(sensorpin,pulsecounter,RISING);
+attachInterrupt(sensorpinSt,pulsecounter,RISING);
+attachInterrupt(sensorpinEn,pulsecounter,RISING);
 
 }
 
 void loop() {
-  
+
 // detach
 if((millis()-oldtime) > 1000){
-  detachInterrupt(sensorpin);
-  flowrate = ((1000.0/(millis()-oldtime))*pulsecount)/calibrationfactor;
+  detachInterrupt(sensorpinSt);
+  detachInterrupt(sensorpinEn);
+  flowrateSt = ((1000.0/(millis()-oldtime))*pulsecount)/calibrationfactor;
+  flowrateEn = ((1000.0/(millis()-oldtime))*pulsecount)/calibrationfactor;
   oldtime = millis();
-  flowMilliliter = (flowrate / 60 ) * 1000;
-
-  totalmilliliters +=flowMilliliter;
-
-  flowliter = flowMilliliter / 1000;
-
-  Serial.print(" Flow rate = ");
-  Serial.print(int(flowrate));
-  Serial.print("L/ Min");
-
-  Serial.print("\t");
-
-  Serial.print("output Liquid quantity = ");
-  Serial.print(totalmilliliters);
-  Serial.print("mL");
-
-  Serial.print("\t");
+  flowMilliliterSt = (flowrateSt / 60 ) * 1000;
+  flowMilliliterEn = (flowrateEn / 60 ) * 1000;
+  totalmillilitersSt +=flowMilliliterSt;
+  totalmillilitersEn +=flowMilliliterEn;
+  flowliterSt = flowMilliliterSt / 1000;
+  flowliterEn = flowMilliliterEn / 1000;
   
-  Serial.print(flowliter);
-  Serial.println("L");
-
+  Serial.print(" Flow rate starting = ");
+  Serial.print(int(flowrateSt));
+  Serial.print("L/ Min");
+  Serial.print("\t");
+  Serial.print("output Liquid quantity Starting = ");
+  Serial.print(totalmillilitersSt/1000);
+  Serial.print("L");
+  Serial.println();
+  Serial.print(" Flow rate Ending = ");
+  Serial.print(int(flowrateEn));
+  Serial.print("L/ Min");
+  Serial.print("\t");
+  Serial.print("output Liquid quantity Ending = ");
+  Serial.print(totalmillilitersEn/1000);
+  Serial.print("L");
+  Serial.println();
+  
   pulsecount = 0;
-  attachInterrupt(sensorpin,pulsecounter,RISING);};
+  attachInterrupt(sensorpinSt,pulsecounter,RISING);
+  attachInterrupt(sensorpinEn,pulsecounter,RISING);};
 }
 
 void pulsecounter (){
